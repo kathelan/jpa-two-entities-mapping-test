53e435d04e447429fd547c518e75ff626b1c36fd
kathelan <mateuszdeptulski@gmail.com>
2024-01-30 23:00:23 +0100

    Initial commit

---
+++ src/main/java/pl/kathelan/jpatwoentitiesmappingtest/JpaTwoEntitiesMappingTestApplication.java
@@ -0,0 +1,13 @@
+package pl.kathelan.jpatwoentitiesmappingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTwoEntitiesMappingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaTwoEntitiesMappingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kathelan/jpatwoentitiesmappingtest/JpaTwoEntitiesMappingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kathelan.jpatwoentitiesmappingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTwoEntitiesMappingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
